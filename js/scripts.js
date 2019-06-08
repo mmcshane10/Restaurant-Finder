2ad46d9d7cca0547277e798e090329cfff18ff4e
Mike <mmcshane10@gmail.com>
2019-06-08 13:09:37 -0700

    add js and css

--- js/scripts.js
+++ js/scripts.js
@@ -6,27 +6,47 @@ $(document).ready(function() {
   });
 
   $(".btn1").click(function() {
-    $(".Q2").show();
+    $(".Q1").fadeOut();
     $(".btn1").hide();
-    $(".Q1").hide();
+    $(".Q2").fadeIn();
   });
 
   $(".btn2").click(function() {
-    $(".Q3").show();
+    $(".Q3").fadeIn();
     $(".btn2").hide();
-    $(".Q2").hide();
+    $(".Q2").fadeOut();
   });
 
   $(".btn3").click(function() {
-    $(".Q4").show();
+    $(".Q4").fadeIn();
     $(".btn3").hide();
-    $(".Q3").hide();
+    $(".Q3").fadeOut();
   });
 
   $(".btn4").click(function() {
-    $(".Q5").show();
+    $(".Q5").fadeIn();
     $(".btn4").hide();
-    $(".Q4").hide();
+    $(".Q4").fadeOut();
   });
 
+  $(".btn5").click(function() {
+    $(".btn5").hide();
+    $(".Q5").hide();
+    $(result).show();
+
+    var type = $("input:radio[name=type]:checked").val();
+    var result;
+
+    if (type === "Italian") {
+      result = ".Italian"
+    } else if (type === "Asian") {
+      result = ".Asian"
+    } else if (type === "American") {
+      result = ".American"
+    } else if (type === "Other") {
+      result = ".Other"
+    }
+
+    console.log();
+  });
 });
